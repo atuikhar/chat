7f025e441ed03707b328f317d3f29a02496cec89
atuikhar <atuikhar@gmail.com>
2022-03-27 18:41:03 +0530

    updated

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+
+app.use(cors());
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User Connected : ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    socket.join(data);
+    console.log(`User with ID : ${socket.id} joined room : ${data}`);
+  });
+
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('receive_message', data);
+  });
+  socket.on('disconnect', () => {
+    console.log(`User Disconnected`, socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server is running...');
+});
